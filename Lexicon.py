762cf9bf9d9e2dc93d924611705828c3f4adfde1
Nicole Crawford <nicolecr@stanford.edu>
2016-05-05 21:30:00 -0700

    TreesAnnotations, Trees
    TreesAnnotations.py done, Trees.py done selectively (only functions
    that seem to be necessary)

---
+++ Lexicon.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+
+class Lexicon:
+	'''Simple default implementation of a lexicon, which scores word,
+	tag pairs with a smoothed estimate of P(tag|word)/P(tag).'''
+
+	# Builds a lexicon from the observed tags in a list of training trees.
+	def __init__(self, trainTrees):
+		self.wordToTagCounters = defaultdict(float)
+		self.totalTokens = 0.0
+		self.totalWordTypes = 0.0
+		self.tagCounter = defaultdict(float)
+		self.wordCounter = defaultdict(float)
+		self.typeTagCounter = defaultdict(float)
+
+		for trainTree in trainTrees:
+			words = trainTree.getYield()
+			tags = trainTree.getPreTerminalYield()
+			for position in xrange(len(words)):
+				word = words[position]
+				tag = tag[position]
+				self.tallyTagging(word, tag)
+
+	def tallyTagging(self, word, tag):
+		if not self.isKnown(word):
+			self.totalWordTypes += 1
+			self.typeTagCounter[tag] += 1
+
+		self.totalTokens += 1
+		self.tagCounter[tag] += 1
+		self.wordCounter[word] += 1
+		self.wordToTagCounters[(word, tag)] += 1
+
+	def isKnown(self, word):
+		return word in self.wordCounter.keys()
+
+	def getAllTags(self):
+		return tagCounter.keys()
+
+	# Returns a smoothed estimate of P(word|tag)
+	def scoreTagging(self, word, tag):
+		p_tag = self.tagCounter[tag] / self.totalTokens
+		c_word = self.wordCounter[word]
+		c_tag_and_word = self.wordToTagCounters[(word, tag)]
+		if c_word < 10: # rare or unknown
+			c_word += 1
+			c_tag_and_word += self.typeTagCounter[tag] / self.totalWordTypes
+
+		p_word = (1.0 + c_word) / (self.totalTokens + self.totalWordTypes)
+		p_tag_given_word = c_tag_and_word / c_word
+		return p_tag_given_word / p_tag * p_word
---
+++ Trees.py
@@ -0,0 +1,95 @@
+class Trees:
+
+	class FunctionNodeStripper:
+		def transformTree(self, tree):
+			transformedLabel = tree.getLabel()
+			cutIndex = transformedLabel.index('-') if '-' in transformedLabel else -1
+			cutIndex2 = transformedLabel.index('=') if '=' in transformedLabel else -1
+			if cutIndex2 > 0 and (cutIndex2 < cutIndex or cutIndex == -1):
+				cutIndex = cutIndex2
+			if cutIndex > 0 and not tree.isLeaf():
+				transformedLabel = transformedLabel[:cutIndex]
+			if tree.isLeaf():
+				return Tree(transformedLabel)
+			transformedChildren = []
+			for child in tree.getChildren():
+				transformedChildren.append(self.transformTree(child))
+
+			return Tree(transformedLabel, transformedChildren)
+
+	class MarkovizationAnnotationStripper:
+		def transformTree(self, tree):
+			transformedLabel = tree.getLabel()
+			caretIndex = transformedLabel.index('^') if '^' in transformedLabel else -1
+			if caretIndex > 0:
+				transformedLabel = transformedLabel[:caretIndex]
+			if tree.isLeaf():
+				return Tree(transformedLabel)
+			transformedChildren = []
+			for child in tree.getChildren():
+				transformedChildren.append(self.transformTree(child))
+			return Tree(transformedLabel, transformedChildren)
+
+	class EmptyNodeStripper:
+		def transformTree(self, tree):
+			label = tree.getLabel()
+			if label == '-NONE-':
+				return None
+			if tree.isLeaf():
+				return Tree(label)
+			transformedChildren = []
+			for child in tree.getChildren():
+				transformedChild = self.transformTree(child)
+				if transformedChild is not None:
+					transformedChildren.append(transformedChild)
+			if len(transformedChildren) == 0:
+				return None
+			return Tree(label, transformedChildren)
+
+	class XOverXRemover:
+		def transformTree(self, tree):
+			label = tree.getLabel()
+			children = tree.getChildren()
+			while len(children) == 1 and not children[0].isLeaf() and label == children[0].getLabel():
+				children = children[0].getChildren()
+			transformedChildren = []
+			for child in children:
+				transformedChildren.append(self.transformTree(child))
+			return Tree(label, transformedChildren)
+
+	class StandardTreeNormalizer:
+		def __init__(self):
+			self.emptyNodeStripper = EmptyNodeStripper()
+			self.xOverXRemover = XOverXRemover()
+			self.functionNodeStripper = FunctionNodeStripper()
+
+		def transformTree(self, tree):
+			tree = self.functionNodeStripper.transformTree(tree)
+			tree = self.emptyNodeStripper.transformTree(tree)
+			tree = self.xOverXRemover.transformTree(tree)
+			return tree
+
+	class TreeReader:
+	# SKIPPING THIS CLASS FOR NOW
+
+	# SKIPPING EVERYTHING AFTER TREE_READER CLASS EXCEPT SPLICE_NODES
+
+	def spliceNodes(self, tree, filtr):
+		rootList = self.spliceNodesHelper(tree, filtr)
+		if len(rootList) > 1:
+			raise Exception('spliceNodes: no unique root after splicing')
+		if len(rootList) < 1:
+			return None
+		return rootList[0]
+
+	def spliceNodesHelper(self, tree, filtr):
+		splicedChildren = []
+		for child in tree.getChildren():
+			splicedChildList = self.spliceNodesHelper(child, filtr)
+			splicedChildren.extend(splicedChildList)
+
+		if filtr(tree.getLabel()):
+			return splicedChildren
+		return [Tree(tree.getLabel(), splicedChildren)]
+
+	# SKIPPING FURTHER FUNCTIONS UNTIL PROVEN NECESSARY
